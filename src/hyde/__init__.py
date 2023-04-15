dcf0b809780dcce736ab2f98992836dc8c64e194
root <root@dgx2-nu-01.issai.local>
2023-04-15 19:37:26 +0000

    fadd hyde

--- src/hyde/__init__.py
+++ src/hyde/__init__.py
@@ -1,3 +1,3 @@
-from .generator import OpenAIGenerator, CohereGenerator
+from .generator import OpenAIGenerator, CohereGenerator, AlpacaGenerator
 from .promptor import Promptor
 from .hyde import HyDE
--- src/hyde/generator.py
+++ src/hyde/generator.py
@@ -1,6 +1,11 @@
 import time
+import torch
 import openai
 import cohere
+
+from transformers import GenerationConfig, LlamaForCausalLM, LlamaTokenizer
+
+from peft import PeftModel
 
 class Generator:
     def __init__(self, model_name, api_key):
@@ -103,4 +108,70 @@ class CohereGenerator(Generator):
                         raise e
             text = self.parse_response(result)
             texts.append(text)
+        return texts
+
+
+class AlpacaGenerator:
+    def __init__(self, base_model, lora_weights, device):
+        self.tokenizer = LlamaTokenizer.from_pretrained(base_model)
+        self.device = device
+        self.n = 5
+        if "cuda" in device:
+            self.model = LlamaForCausalLM.from_pretrained(
+                base_model,
+                load_in_8bit=True,
+                torch_dtype=torch.float16,
+                device_map="auto",
+            )
+            self.model = PeftModel.from_pretrained(
+                self.model,
+                lora_weights,
+                torch_dtype=torch.float16,
+            )
+        
+        self.model.config.pad_token_id = self.tokenizer.pad_token_id = 0
+        self.model.config.bos_token_id = 1
+        self.model.config.eos_token_id = 2
+
+        self.model.eval()
+
+        self.temperature=0.1
+        self.top_p=0.75
+        self.top_k=40
+        self.num_beams=4
+        self.max_new_tokens=64
+
+    def generate(self, prompt, **kwargs):
+        inputs = self.tokenizer(prompt, return_tensors="pt")
+        input_ids = inputs["input_ids"].to(self.device)
+        texts = []
+        for _ in range(self.n):
+            generation_config = GenerationConfig(
+                temperature=self.temperature,
+                top_p=self.top_p,
+                top_k=self.top_k,
+                num_beams=self.num_beams,
+                **kwargs,
+            )
+            generate_params = {
+                "input_ids": input_ids,
+                "generation_config": generation_config,
+                "return_dict_in_generate": True,
+                "output_scores": True,
+                "max_new_tokens": self.max_new_tokens,
+            }
+
+            with torch.no_grad():
+                generation_output = self.model.generate(
+                    input_ids=input_ids,
+                    generation_config=generation_config,
+                    return_dict_in_generate=True,
+                    output_scores=True,
+                    max_new_tokens=self.max_new_tokens,
+                )
+            s = generation_output.sequences[0]
+            output = self.tokenizer.decode(s)
+            #response = output.split('###Passage:')[1].strip()
+            texts.append(output)
+
         return texts
